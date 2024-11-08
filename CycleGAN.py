744064d4bcb7870b695aaeefeb9bdf2498f89f5a
boran-kilic <165495405+boran-kilic@users.noreply.github.com>
2024-11-08 22:25:44 +0300

    Update CycleGAN.py

--- CycleGAN.py
+++ CycleGAN.py
@@ -1,2 +1,12 @@
 #CycleGAN
 import tensorflow as tf
+
+import tensorflow_datasets as tfds
+from tensorflow_examples.models.pix2pix import pix2pix
+
+import os
+import time
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+AUTOTUNE = tf.data.AUTOTUNE
