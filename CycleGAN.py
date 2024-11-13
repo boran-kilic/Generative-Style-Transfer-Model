54c3516074bf42e786438641e059e409ad3f8534
boran-kilic <165495405+boran-kilic@users.noreply.github.com>
2024-11-14 02:40:32 +0300

    Update CycleGAN.py

--- CycleGAN.py
+++ CycleGAN.py
@@ -69,13 +69,10 @@ def random_jitter(image):
   # resizing to 286 x 286 x 3
   image = tf.image.resize(image, [286, 286],
                           method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-
   # randomly cropping to 256 x 256 x 3
   image = random_crop(image)
-
   # random mirroring
   image = tf.image.random_flip_left_right(image)
-
   return image
 
 
